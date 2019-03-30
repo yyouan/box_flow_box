9eeca9c3e7ccbf402d71c6c7bca2ead54f450c7d
yyouan <b05202030@ntu.edu.tw>
2019-03-31 04:26:44 +0800

    deposit

--- main.py
+++ main.py
@@ -19,28 +19,7 @@ def button():
         return False
 
 
-#收到server傳來的message的callback
-def onMessage(ws, msg):
-    print("收到了從server傳來的message：" + msg)
-    msg = json.loads(msg)
-    
 
-#與server建立連線後的callback
-def onOpen(ws):
-    sendMessage = '安安我是python'
-    ws.send(sendMessage)
-
-#server的HOST
-HOST = 'ws://boxflow.herokuapp.com:12345'
-
-#與server約定好的protocol server端也要設定
-SUBPROTOCOLS = ['echo-protocol']
-
-#建立websocket
-ws = websocket.WebSocketApp(HOST, subprotocols = SUBPROTOCOLS, on_open = onOpen, on_message = onMessage)
-
-#一直執行
-#ws.run_forever()
 
 while True:
     print("no")
@@ -89,5 +68,6 @@ while True:
             
             
         msg = {"deposit":ans ,"box_id":boxid}
-        ws.send(json.dumps(msg))
+        headers = {'Content-Type': 'application/json'} 
+        response_2 = requests.post("https://boxflow.herokuapp.com/deposit", params=params, headers=headers, json = msg)
         print(json.dumps(msg))
