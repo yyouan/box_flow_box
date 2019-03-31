c001bef682981b57d1f42a10b7706ae5ed3790a9
yyouan <b05202030@ntu.edu.tw>
2019-03-31 10:36:04 +0800

    withdraw

--- main.py
+++ main.py
@@ -84,12 +84,13 @@ while True:
                         ans = resul["text"]
                 if ans =="":                    
                     response_2 = requests.post("https://boxflow.herokuapp.com/nodeposit")             
+                else:
                 servonew.turnonecircle()
                 print("ID:",ans)
                 print("rotate!")
+                msg = {"deposit":ans ,"box_id":boxid}
+                headers = {'Content-Type': 'application/json'} 
+                response_2 = requests.post("https://boxflow.herokuapp.com/deposit", params=params, headers=headers, json = msg)
+                print(json.dumps(msg))
                 
-                
-            msg = {"deposit":ans ,"box_id":boxid}
-            headers = {'Content-Type': 'application/json'} 
-            response_2 = requests.post("https://boxflow.herokuapp.com/deposit", params=params, headers=headers, json = msg)
-            print(json.dumps(msg))
+            
